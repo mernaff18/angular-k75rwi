2b66b4a285731da3c5957fcccd1d4567f11bcd04
mernaff18 <roysmernaff@gmail.com>
2019-05-09 19:31:53 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -103,5 +103,35 @@ registerForm: FormGroup;
 
         console.log("success" + this.registerForm.value.firstName);
     }
-  
+  ps: number = 1;
+  correctAns : any ;
+  items: Array<string> = [];
+
+ 
+  print() {
+    console.log(this.questionsanswer)
+  }
+  questionsanswer=[
+    {
+      "number": 1,
+      "question": "Eighteen thousandths, written as a decimal, is:",
+      "answers": [
+        "0.0018",
+        "0.018",
+        "0.18"
+      ],
+      "correct_answer": 1
+    },
+    {
+      "number": 2,
+      "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
+      "answers": [
+        "12",
+        "13",
+        "14",
+        "15"
+      ],
+      "correct_answer": 4
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {MatGridListModule,MatCardModule,MatChipsModule,MatInputModule,  MatFormFieldModule} from '@angular/material';
+import {MatRadioModule,MatGridListModule,MatCardModule,MatChipsModule,MatInputModule,  MatFormFieldModule} from '@angular/material';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MatGridListModule,MatCardModule,MatChipsModule,FlexLayoutModule,NgxPaginationModule,HttpClientModule,ReactiveFormsModule,MatInputModule,  MatFormFieldModule],
+  imports:      [ MatRadioModule,BrowserModule, FormsModule,MatGridListModule,MatCardModule,MatChipsModule,FlexLayoutModule,NgxPaginationModule,HttpClientModule,ReactiveFormsModule,MatInputModule,  MatFormFieldModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
