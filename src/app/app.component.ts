a2353c32eefd2f3cb3947a03cec8fbf0ec960a7b
mernaff18 <roysmernaff@gmail.com>
2019-05-10 15:59:40 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -113,13 +113,38 @@ registerForm: FormGroup;
   choosedAnswer:any = [];
   score : any = 0;
   answerOnselect(questionNo,correctAnswer,choosedAnswer){ 
-        this.myAnswers.push(
-          { 
-            "questionNo":questionNo,
-            "correctAnswer":correctAnswer,
-            "choosedAnswer":choosedAnswer.value
+
+      if(this.myAnswers.length<=0){
+            this.myAnswers.push(
+              { 
+                "questionNo":questionNo,
+                "correctAnswer":correctAnswer,
+                "choosedAnswer":choosedAnswer.value
+              }
+          );
+      }
+      else
+      {
+          for(let i =0; i<this.myAnswers.length;i++){
+              if(this.questionNo[i] = questionNo)
+              {
+
+              }
+              else
+              {
+                this.myAnswers.push(
+                    { 
+                      "questionNo":questionNo,
+                      "correctAnswer":correctAnswer,
+                      "choosedAnswer":choosedAnswer.value
+                    }
+                );
+              }
+              
           }
-      );
+
+      }
+        
   }
  
   submitExam() {
