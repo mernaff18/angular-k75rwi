bcc690b8313e1a869c401eb6c70a9c3af85f499f
mernaff18 <roysmernaff@gmail.com>
2019-05-12 12:10:31 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatRadioModule,MatGridListModule,MatCardModule,MatChipsModule,MatInputModule,  MatFormFieldModule} from '@angular/material';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ MatRadioModule,BrowserModule, FormsModule,MatGridListModule,MatCardModule,MatChipsModule,FlexLayoutModule,NgxPaginationModule,HttpClientModule,ReactiveFormsModule,MatInputModule,  MatFormFieldModule],
+  imports:      [ MatRadioModule,BrowserModule, BrowserAnimationsModule,FormsModule,MatGridListModule,MatCardModule,MatChipsModule,FlexLayoutModule,NgxPaginationModule,HttpClientModule,ReactiveFormsModule,MatInputModule,  MatFormFieldModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
